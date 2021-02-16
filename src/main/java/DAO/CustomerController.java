f98fc40f2917a38814b2d9aeed69593ae79b6059
jordanken <douanlacompanie@gmail.com>
2021-02-16 16:20:18 +0100

    mvc

--- src/main/java/DAO/CustomerController.java
+++ src/main/java/DAO/CustomerController.java
@@ -3,8 +3,12 @@ package DAO;
 
 import javax.servlet.http.HttpServletRequest;
 import model.Customer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
